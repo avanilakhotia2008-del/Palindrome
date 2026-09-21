b6e4cc45eb746c1a2c70f87405127a9db5e91577
Avani Lakhotia <avani.lakhotia2008@gmail.com>
2026-09-21 04:29:22 +0100

    added more comments

--- palindrome.cpp
+++ palindrome.cpp
@@ -5,40 +5,38 @@ using namespace std;
 
 int main()
 {
-    // array can hold up to 80 characters plus the null character
-    char input[81];
-    char cleaned[81];
-    char backwards[81];
+    // holds 80 characters + null character
+  char input[81]; //creates character array called input 
+  char cleaned[81]; // creates character array called cleaned
+  char backwards[81]; //creates character array called backwards 
 
-    cout << "Enter a series of characters (80 characters max): ";
-    cin.getline(input, 81);
+    cout << "Enter a series of characters (80 characters max): "; //entry for user
+    cin.getline(input, 81); //reads the line and stores it inside input 
 
     int length = 0;
 
-    // this remove spaces and punctuation and make all letters lowercase
+    // remove spaces and punctuation
     for (int i = 0; input[i] != '\0'; i++)
     {
         if (isalnum(input[i]))
         {
-            cleaned[length] = tolower(input[i]);
+	  cleaned[length] = tolower(input[i]); // puts characters into cleaned and lowercase
             length++;
         }
     }
 
-    // adds the null character to the end of the cleaned array
     cleaned[length] = '\0';
 
-    // this puts the cleaned array into backwards order
+    // reverse the cleaned string
     for (int i = 0; i < length; i++)
     {
-        backwards[i] = cleaned[length - 1 - i];
+      backwards[i] = cleaned[length - 1 - i];  //taking characters from cleaned and putting them into backwards 
     }
 
-    // adds the null character to the backwards array
     backwards[length] = '\0';
 
-    // compares the original cleaned array to the backwards array
-    if (strcmp(cleaned, backwards) == 0)
+    // check if they're the same
+    if (strcmp(cleaned, backwards) == 0) // finally compares the string 
     {
         cout << "Palindrome." << endl;
     }
